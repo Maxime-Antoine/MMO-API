fa9a3bfb51cd9408a8a9b9eb964e93b1d4ac888e
Maxime <maxime.antoine2@gmail.com>
2016-12-02 01:06:15 +0800

    updated POST: /login

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 	var port = process.env.PORT || 3000;
 	var app = require('express')();
 	var bodyParser = require('body-parser');
+	
+	//parse application/json
 	app.use(bodyParser.json());
 	
 	app.get('/', function(req, res){
@@ -11,11 +13,17 @@
 	app.post('/login', function(req, res){
 		var login = req.body.login;
 		var pwd = req.body.pwd;
+		var resContent = {};
 		
-		if (login === "Max" && pwd === "123")
-			res.send('OK');
-		else
-			res.send('Error');
+		if (login === "Max" && pwd === "123"){
+			resContent.result = 'OK';
+			resContent.playerId = 'eWRhpRV';
+		}
+		else {
+			resContent.result = 'NOK';
+			resContent.reason = 'Incorrect credentials';
+		}
+		res.json(resContent);
 	})
 	
 	app.listen(port, function(){
