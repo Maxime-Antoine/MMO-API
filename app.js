cebf5bb8cc43568b01b92b1fbee9fa515d1f5048
Maxime <maxime.antoine2@gmail.com>
2017-04-28 18:53:27 +0800

    removed useless closure

--- app.js
+++ app.js
@@ -1,80 +1,78 @@
-(function(){
-	var port = process.env.PORT || 3000;
-	var app = require('express')();
-	var bodyParser = require('body-parser');
-	var shortId = require('shortid');
+var port = process.env.PORT || 3000;
+var app = require('express')();
+var bodyParser = require('body-parser');
+var shortId = require('shortid');
+
+//parse application/json
+app.use(bodyParser.json());
+
+//temp memory storage of users accounts
+var accounts = [ 
+	{ login: 'Max', pwd: '123', email: 'max@test.com', playerId: 'eWRhpRV' },
+	{ login: 'Max2', pwd: '123', email: 'max@test.com', playerId: 'fWRhpRV'}
+];
+
+// -------- Routes --------
+app.get('/', function(req, res){
+	res.send('Nothing here');
+})
+
+app.post('/login', function(req, res){
+	var login = req.body.login;
+	var pwd = req.body.pwd;
+	var resContent = {};
 	
-	//parse application/json
-	app.use(bodyParser.json());
-	
-	//temp memory storage of users accounts
-	var accounts = [ 
-		{ login: 'Max', pwd: '123', email: 'max@test.com', playerId: 'eWRhpRV' },
-		{ login: 'Max2', pwd: '123', email: 'max@test.com', playerId: 'fWRhpRV'}
-	 ];
-	
-	// -------- Routes --------
-	app.get('/', function(req, res){
-		res.send('Nothing here');
+	var player = null;
+	accounts.forEach(function(a){
+		if (a.login == login && a.pwd == pwd)
+			player = a;
 	})
 	
-	app.post('/login', function(req, res){
-		var login = req.body.login;
-		var pwd = req.body.pwd;
-		var resContent = {};
-		
-		var player = null;
-		accounts.forEach(function(a){
-			if (a.login == login && a.pwd == pwd)
-				player = a;
-		})
-		
-		if (player){
-			console.log('Login request for ' + login + ' - ' + pwd + ' succeeded');
-			resContent.result = 'OK';
-			resContent.playerId = player.playerId;
-			resContent.playerName = player.login;
-		}
-		else {
-			console.log('Login request for ' + login + ' - ' + pwd + 'failed');
-			resContent.result = 'NOK';
-			resContent.reason = 'Incorrect credentials';
-		}
-		res.json(resContent);
-	})
+	if (player){
+		console.log('Login request for ' + login + ' - ' + pwd + ' succeeded');
+		resContent.result = 'OK';
+		resContent.playerId = player.playerId;
+		resContent.playerName = player.login;
+	}
+	else {
+		console.log('Login request for ' + login + ' - ' + pwd + 'failed');
+		resContent.result = 'NOK';
+		resContent.reason = 'Incorrect credentials';
+	}
+	res.json(resContent);
+})
+
+app.get('/username', function(req, res){
+	var result = {};
+	result.names = [];
 	
-	app.get('/username', function(req, res){
-		var result = {};
-		result.names = [];
-		
-		console.log('Usernames requested');
-		
-		accounts.forEach(function(a){
-			result.names.push(a.login);
-		});
-		
-		res.json(result);
-	})
+	console.log('Usernames requested');
 	
-	app.post('/signup', function(req, res){		
-		var login = req.body.login;
-		var pwd = req.body.pwd;
-		var email = req.body.email;
-		var playerId = shortId.generate();
-		
-		console.log('Signup: ' + login + ', ' + pwd + ', ' + email + ', ' + playerId);
-		
-		accounts.push({ 
-			login: login, 
-			pwd: pwd, 
-			email: email, 
-			playerId: playerId 
-		});
-		
-		res.json({ login: login, playerId: playerId });
-	})
+	accounts.forEach(function(a){
+		result.names.push(a.login);
+	});
 	
-	app.listen(port, function(){
-		console.log('express started');
-	})
-})()
+	res.json(result);
+})
+
+app.post('/signup', function(req, res){		
+	var login = req.body.login;
+	var pwd = req.body.pwd;
+	var email = req.body.email;
+	var playerId = shortId.generate();
+	
+	console.log('Signup: ' + login + ', ' + pwd + ', ' + email + ', ' + playerId);
+	
+	accounts.push({ 
+		login: login, 
+		pwd: pwd, 
+		email: email, 
+		playerId: playerId 
+	});
+	
+	res.json({ login: login, playerId: playerId });
+})
+
+app.listen(port, function(){
+	console.log('express started');
+})
